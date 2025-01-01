a112196e3bac6543cb3235638365cecc3d35b2a6
CodeAphex <codeaphex@outlook.com>
2025-01-01 17:31:52 +0100

    Add project files.

---
+++ HelloPlugin/HelloCommand.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PluginBase;
+
+namespace HelloPlugin
+{
+    public class HelloCommand : ICommand
+    {
+        private readonly IConfiguration? _configuration;
+        private readonly IServiceProvider? _serviceProvider;
+
+        // This ctor fails immediately
+        //public HelloCommand(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //    var baseUrl = configuration["Plugins:HOU:BaseUrl"];
+        //}
+
+        public HelloCommand(IServiceProvider serviceProvider)
+        {
+            // IConfiguration seems to be listed in descriptors
+            // But fails when trying to retrieve an instance
+            _serviceProvider = serviceProvider;
+            //_configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        }
+
+        public string Name { get => "hello"; }
+        public string Description { get => "Displays hello message."; }
+
+        public int Execute()
+        {
+            Console.WriteLine("Hello !!!");
+            return 0;
+        }
+    }
+}
---
+++ pluginIConfigRepro/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PluginBase;
+using System.Reflection;
+
+namespace pluginIConfigRepro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);            
+
+            try
+            {
+                List<string> pluginPaths =
+                [                
+                    @"HelloPlugin\bin\Debug\net9.0\HelloPlugin.dll"
+                ];
+
+                pluginPaths.ForEach((pluginPath) =>
+                {
+                    Assembly pluginAssembly = LoadPlugin(pluginPath);
+                    RegisterCommands(pluginAssembly, builder);
+                });
+                var host = builder.Build();
+
+                var configuration = host.Services.GetRequiredService<IConfiguration>();
+                Console.WriteLine($"ApplicationName from config:\t {configuration["ApplicationName"]}");
+
+                var commandServices = host.Services.GetServices<ICommand>().ToList();
+
+                Console.WriteLine("Commands: ");
+                commandServices.ForEach((command) =>
+                {
+                    Console.WriteLine($"{command.Name}\t - {command.Description}");
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static Assembly LoadPlugin(string relativePath)
+        {
+            // Navigate up to the solution root
+            string root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(
+                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
+
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            Console.WriteLine($"Loading assembly from: {pluginLocation}");
+            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }  
+
+        static void RegisterCommands(Assembly assembly, HostApplicationBuilder builder)
+        {
+            int count = 0;
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(ICommand).IsAssignableFrom(type))
+                {
+                    Console.WriteLine($"Registering type: {type}");
+                    builder.Services.AddSingleton(typeof(ICommand), type);
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                throw new ApplicationException(
+                    $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                    $"Available types: {availableTypes}");
+            }
+        }
+    }
+}
